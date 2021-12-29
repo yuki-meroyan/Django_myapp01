462a98d22b25dbe8e741783c00b50ea3afc4c845
yuki-meroyan <yakan.meroyan@gmail.com>
2021-12-29 16:14:07 +0900

    create static

--- votes/views.py
+++ votes/views.py
@@ -30,7 +30,7 @@ from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = 'votes/index.html'
-    context_object_name = 'latest_qustion_list'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
